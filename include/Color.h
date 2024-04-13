5a7c4ac43c834b3ae0a27996951cffd756411c7b
MattFor <matthew-forester@protonmail.com>
2024-04-13 19:09:11 +0200

    Starting up.

---
+++ include/Color.h
@@ -0,0 +1,14 @@
+//
+// Created by MattFor on 13.04.2024.
+//
+
+#ifndef RAYTRACER_COLOR_H
+#define RAYTRACER_COLOR_H
+
+
+#include "Vec3.h"
+
+class Color : public Vec3 {};
+
+
+#endif //RAYTRACER_COLOR_H
